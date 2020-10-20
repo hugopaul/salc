7061ee3a8779d5fee239c66b6260c0963dbf2c63
Hugo Paul <hugo.4500@icloud.com>
2020-10-20 16:46:58 -0300

    Criando projeto da SALC

---
+++ src/main/java/br/mil/eb/basecmp/salc/SalcApplication.java
@@ -0,0 +1,14 @@
+package br.mil.eb.basecmp.salc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalcApplication.class, args);
+	}
+
+}
